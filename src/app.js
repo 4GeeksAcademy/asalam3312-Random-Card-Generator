c3d5e4a935b16a6fcb06e937806330e378c3f724
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2024-02-29 04:20:17 +0000

    basic card created

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let cardType = ["♦", "♥", "♠", "♣"];
+
+  let randomNumber = Math.floor(Math.random() * cardNumber.length);
+
+  let number = document.getElementById("number");
+  number.value = cardNumber[randomNumber];
+  number.innerHTML = cardNumber[randomNumber];
+  number.appendChild = cardNumber[randomNumber];
+
+  let randomType = Math.floor(Math.random() * cardType.length);
+  console.log(cardNumber[randomNumber], cardType[randomType]);
+
+  let type = document.getElementById("type");
+  type.value = cardType[randomType];
+  type.innerHTML = cardType[randomType];
+  type.appendChild = cardType[randomType];
+
+  let type2 = document.getElementById("type2");
+  type2.value = cardType[randomType];
+  type2.innerHTML = cardType[randomType];
+  type2.appendChild = cardType[randomType];
+
+  /* if () */
 };
